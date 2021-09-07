093f6b99823ae177cfae4094d3385412ce661702
Kristian-j-k <89967661+Kristian-j-k@users.noreply.github.com>
2021-09-07 10:45:23 +0200

    count second

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// tæller sekunder siden midnat, er ikke opdateret
+       int hour = 10;
+       int hourSec =hour*60*60;
+       int minute = 22;
+       int minuteSec =minute*60;
+       int second = 40;
+
+
+
+        int siden;
+        siden = second+minuteSec +hourSec;
+        int til;
+      til =24*60*60-second-minuteSec+hourSec;
+
+    // tæller sekunder siden midnat
+        System.out.print(siden);
+        System.out.println(" sekunder siden midnat");
+
+    // tæller sekunder til midnat
+        System.out.print(til);
+        System.out.println(" sekunder til midnat");
+
+    //percentage of the day
+        double helDag = 24*60*60;
+        System.out.print(helDag/til*100);
+        System.out.println(" procent tilbage af dagen");
+    }
+}
